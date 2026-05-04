fd5396bd0fee81d6e8f26b558a4b684e7b6f758b
TenzDelek <tibetdelek@gmail.com>
2026-05-04 16:12:46 +0530

    port binding

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts:true
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

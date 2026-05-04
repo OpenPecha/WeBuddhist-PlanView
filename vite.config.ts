920b2301e4ed7c42eb8aab40d43379ad1b961acd
TenzDelek <tibetdelek@gmail.com>
2026-05-04 16:25:07 +0530

    proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,32 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    allowedHosts:true
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "")
+  const backendBaseUrl = env.VITE_BACKEND_BASE_URL
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      open: true,
+      allowedHosts: true,
+      proxy: {
+        "/api": {
+          target: backendBaseUrl,
+          changeOrigin: true,
+        },
+      },
+    },
+    preview: {
+      host: "0.0.0.0",
+      port: 4173,
+    },
+  }
 })

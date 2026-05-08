b252b299d40438e68d5e9efd4044f5883002ee30
TenzDelek <tibetdelek@gmail.com>
2026-05-08 11:29:32 +0530

    posthog init

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,12 @@ import { BrowserRouter } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import "./index.css"
 import App from "./App.tsx"
+import { PostHogProvider } from '@posthog/react'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  defaults: '2026-01-30',
+} as const
 
 const queryClient = new QueryClient()
 
@@ -11,7 +17,9 @@ createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
+        <PostHogProvider apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_PROJECT_TOKEN} options={options}>
+          <App />
+        </PostHogProvider>
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
